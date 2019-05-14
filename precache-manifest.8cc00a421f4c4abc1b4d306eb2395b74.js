ba2c83aa1c804b7f2a7ced383ee46f47033e6312
Erik Sommer <ersotech@posteo.de>
2019-05-14 22:18:49 +0200

    Updates

---
+++ precache-manifest.8cc00a421f4c4abc1b4d306eb2395b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af484fb9e7f0234e1da1c3cfc16015d6",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "a63d7e32d7103e26a651",
+    "url": "/react_test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a63d7e32d7103e26a651",
+    "url": "/react_test/static/js/main.b06fa373.chunk.js"
+  },
+  {
+    "revision": "37c519b1b0bef28b0825",
+    "url": "/react_test/static/js/runtime~main.13abe56d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
